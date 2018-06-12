76eeaf92749cd11a2c2b4c7ccc2b6b0eff9b0742
Brandon Bennett <brandonabennett24@gmail.com>
2018-06-12 16:07:19 -0600

    Versioning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GenTimer.MixProject do
   def project do
     [
       app: :gen_timer,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
